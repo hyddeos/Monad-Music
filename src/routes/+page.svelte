e112cbaab4a68cfff00e4476370498c88a961887
hyddeos <ericsageby@hotmail.com>
2023-03-28 13:07:51 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
+<script>
+
+</script>
+
 <section>
-    <div class="text-center ">
-        <h2 class="text-3xl font-ingress font-thin italic tracking-wider">Lust Lera Leva</h2>
+    <div class="text-center">
+        <section class="flex flex-row max-w-5xl m-auto mt-80">
+            <div class="basis-1/3 h-40 m-auto border-r-2 border-prim-400"><img src="https://flowbite.com/docs/images/logo.svg" class="h-32" alt="Flowbite Logo" /></div>
+            <div class="basis-2/3 h-40 ">
+                <h1 class="text-8xl text-right">Anettes Keramik<h1>
+                <h3 class="text-4xl text-right font-ingress font-thin italic tracking-wider"> - Lust Lera Leva</h3>
+            </div>
+        </section>  
     </div>
 </section>
