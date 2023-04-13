c8b484ac2af841ab5de12ac0e6c6bf594cee3901
hyddeos <ericsageby@hotmail.com>
2023-04-13 13:49:18 +0200

    bg in place

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,53 @@
   import { fade } from 'svelte/transition';
 
   let bgImage = "/bg.jpg";
+  // Spotlight1
+  let x1 = -200;
+  let y1 = -200;
+  let s1 = false;
+  // Spotlight2
+  let x2 = -200;
+  let y2 = -200;
+  let s2 = false;
+  // Spotlight3
+  let x3 = -200;
+  let y3 = -200;
+  let s3 = false;
 
-  let x = 0;
-  let y = 0;
-
-  function updatePosition() {
+  function updatePosition1() {
     const maxX = window.innerWidth;
     const maxY = window.innerHeight;
-    x = Math.floor(Math.random() * maxX + 100);
-    y = Math.floor(Math.random() * maxY + 100);
+    x1 = Math.floor(Math.random() * maxX);
+    y1 = Math.floor(Math.random() * maxY);
+  }
+
+  function updatePosition2() {
+    const maxX = window.innerWidth;
+    const maxY = window.innerHeight;
+    x2 = Math.floor(Math.random() * maxX);
+    y2 = Math.floor(Math.random() * maxY);
+  }
+
+  function updatePosition3() {
+    const maxX = window.innerWidth;
+    const maxY = window.innerHeight;
+    x3 = Math.floor(Math.random() * maxX + 100);
+    y3 = Math.floor(Math.random() * maxY + 100);
   }
 
   onMount(() => {
     const interval = setInterval(() => {
-      updatePosition();
-    }, 4000);
+      updatePosition1();
+      s1 = true;
+    }, 3000);
+    const interval2 = setInterval(() => {
+      updatePosition2();
+      s2 = true;
+    }, 6000);
+    const interval3 = setInterval(() => {
+      updatePosition3();
+      s3 = true;
+    }, 10000);
 
     return () => clearInterval(interval);
   });
@@ -31,8 +63,10 @@
   
   <img id="bg" src="{bgImage}" alt="consert background">
   <div class="bg-dark-900 w-full h-full fixed -z-30"></div>
-
-  <div id="spotlight1" transition:fade style="transform: translate({x}px, {y}px"></div>  
+  <div id="spotlight1" transition:fade style="transform: translate({x1}px, {y1}px; visibility: {s1 ? "visible" : "hidden"};"></div>
+  <div id="spotlight2" transition:fade style="transform: translate({x2}px, {y2}px; visibility: {s2 ? "visible" : "hidden"};"></div>
+  <div id="spotlight3" transition:fade style="transform: translate({x3}px, {y3}px; visibility: {s3 ? "visible" : "hidden"};"></div>
+  
 
     <section class="flex flex-row max-w-5xl m-auto pt-[20%] z-0">
       <div class="basis-1/3 h-40 m-auto border-r-2 border-prim-400">
@@ -67,6 +101,30 @@
     filter: blur(1.5rem);
     border-radius: 100%;
     transition: transform 6s ease-out;
+  }
+  #spotlight2 {
+    mix-blend-mode: color-dodge;
+    opacity: 0.3;
+    z-index: -1;
+    position: absolute;
+    width: 35rem;
+    height: 35rem;
+    background-color: pink;
+    filter: blur(1.5rem);
+    border-radius: 100%;
+    transition: transform 6s ease-out;
+  }
+  #spotlight3 {
+    mix-blend-mode: color-dodge;
+    opacity: 0.2;
+    z-index: -1;
+    position: absolute;
+    width: 35rem;
+    height: 35rem;
+    background-color: yellow;
+    filter: blur(1.5rem);
+    border-radius: 100%;
+    transition: transform 10s ease-out;
   }
   #bg {
     z-index: -2;
