e994483ff3f3fea5b6f799c2f58c791810751847
hyddeos <ericsageby@hotmail.com>
2023-04-14 20:16:06 +0200

    footer fixed

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,13 +4,18 @@
 
 <footer class="absolute bottom-0 w-full">
   <div
-    class="flex flex-wrap text-center font-head font-thin  justify-evenly text-light-500 m-auto"
+    class="flex flex-wrap text-center font-head font-thin justify-evenly text-light-200 m-auto bg-dark-900 opacity-50"
   >
     <div class="m-2">
-      Monad Music © {year}
+      <span class="font-bold">Monad Music</span> © {year}
     </div>
-    <div class="m-2">Adress: stallaromgen fkkfoekf</div>
-    <div class="m-2">Kontakt: epost@mail.nu</div>
-    <div class="m-2">Sidan skapad av: Grafoma</div>
+    <div class="m-2">
+      Made by <a href="https://eshtropy.se/" class="font-bold">Esh</a>
+    </div>
+    <div class="m-2">
+      Visit me at <a href="https://github.com/hyddeos" class="font-bold"
+        >Github</a
+      >
+    </div>
   </div>
 </footer>
