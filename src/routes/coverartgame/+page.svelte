8d55fe7675d48c6ba555f45c036145bb59b2fd2c
hyddeos <ericsageby@hotmail.com>
2023-04-18 16:41:32 +0200

    Game design added

--- src/routes/coverartgame/+page.svelte
+++ src/routes/coverartgame/+page.svelte
@@ -12,56 +12,58 @@
   //
   // LAYOUT FOR GAME
   //
-  let albumcover = "https://www.bengans.se/bilder/artiklar/560029.jpg"
-  let album_guess = 0 ;
+  let game_loaded = false;
+  let playlist = '';
+  let albumcover = "https://www.bengans.se/bilder/artiklar/560029.jpg";
+  let album_guess = 99;
 
   // Temp varibles
-  let album_1 = "Cult of Luna - Vertikal"
-  let album_2 = "Amenra - Mass III"
-  let album_3 = "Godspeed You! Black Emperor - Lift Your skinny fist"
-  let album_4 = "Abba - Mamma Mia"
-  let album_5 = "Fall of Efrafa - Elil" 
   let albums = [
-		{ id: 1, artist: 'Cult of Luna', album: 'Somewhere Alone The Highway' },
-		{ id: 2, artist: 'Amenra', album: 'Mass III' },
-		{ id: 3, artist: 'Godspeed You! Black Emperor', album: 'Lift your skinny fist like antennas to the sky' },
-		{ id: 4, artist: 'Fall of Efrafa', album: 'Elil' },
-		{ id: 6, artist: 'Show Me A Dinosour', album: 'Reke' },
+		{ id: 0, artist: 'Cult of Luna', album: 'Somewhere Along The Highway' },
+		{ id: 1, artist: 'Amenra', album: 'Mass III' },
+		{ id: 2, artist: 'Godspeed You! Black Emperor', album: 'Lift your skinny fist like antennas to the sky' },
+		{ id: 3, artist: 'Fall of Efrafa', album: 'Elil' },
+		{ id: 4, artist: 'Show Me A Dinosour', album: 'Reke' },
 	];
 
   function set_active_guess(album_id) {
     album_guess = album_id;
-    console.log("Current Guess:", album_guess)
   }
 
 </script>
 
-<section class="flex">
-  <div class="m-30 mx-auto w-full">
+<section id="main_content" class="flex">
+  <div class="m-30 mx-auto w-full ">
+
     <button on:click={login}>Log in with Spotify</button>
     <p>Current Guess: {album_guess}</p>
-    <h2 class="text-4xl text-center font-handwrite tracking-wider">What Album Is This? <span class="text-2xl text-light-300 text-right">(1/10)</span></h2>
-    <img class="m-auto p-5" src={albumcover} alt="Guess this album cover">
-    <div class="border border-sec-500 w-full flex justify-center">
-      <button on:click={() =>  set_active_guess(album_1)} class="w-1/5 bg-dark-700 text-center mx-3 h-40">
-        {album_1}
-      </button>
-      <button on:click={() =>  set_active_guess(album_2)} class="w-1/5 bg-dark-700 text-center mx-3">
-        {album_2}
-      </button>
-      <button on:click={() =>  set_active_guess(album_3)} class="w-1/5 bg-dark-700 text-center mx-3">
-        {album_3}
-      </button>
-      <button on:click={() =>  set_active_guess(album_4)} class="w-1/5 bg-dark-700 text-center mx-3">
-        {album_4}
-      </button>
-      <button on:click={() => set_active_guess(album_5)} class="w-1/5 bg-dark-700 text-center mx-3">
-        {album_5}
-      </button>
-    </div>
+    <p>Current PL: {playlist}</p>
+
+    {#if game_loaded}
+      <h2 class="text-5xl text-center font-handwrite tracking-wider">What Album Is This? <span class="text-2xl text-light-300 text-right">(1/10)</span></h2>
+      <img class="m-auto p-5 w-1/5" src={albumcover} alt="Guess this album cover">
+      <div class="w-full flex flex-wrap justify-center px-2">  
+        {#each albums as album}
+        <div class="p-2">
+          <button on:click={() => set_active_guess(album.id)} class='w-80 h-40 rounded {album_guess === album.id ? "bg-dark-200 text-dark-700" : "bg-dark-700"} text-center text-ellipsis overflow-hidden hover:bg-dark-100 hover:text-dark-700'>
+            <h3 class="font-bold text-xl p-1">{album.album}<h3>
+            <h4 class="font-thin p-1">{album.artist}</h4>
+          </button>
+        </div>
+        {/each}
+      </div>
+      <div class="m-auto text-center">
+        <button class="w-40 h-20 rounded {album_guess !== 99 ? "bg-prim-500 text-dark-700" : "invisible"}  hover:bg-dark-100">Submit</button>
+      </div>  
+    {:else}
+      <h2 class="text-5xl text-center font-handwrite tracking-wider">Load Your Playlist!</h2>
+      <p>Game not loaded</p>
+      <input bind:value={playlist}>
+    {/if}
 
   </div>
 </section>
 
 <style>
+
 </style>
