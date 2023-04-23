abc509b99770b87f44ff1f2476e22bbfd0aae65d
Eric <39992041+hyddeos@users.noreply.github.com>
2023-04-23 21:28:34 +0200

    Working login auth flow (#1)

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
+
+  const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+  const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+  const client_refresh_token = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN;
+  const redirectUri = "http://localhost:5173/auth/code";
+
+  function generateRandomString(length) {
+    let text = "";
+    let possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+  async function sha256(data) {
+    const encoder = new TextEncoder();
+    const buffer = encoder.encode(data);
+    const digest = await crypto.subtle.digest("SHA-256", buffer);
+    const hashArray = Array.from(new Uint8Array(digest));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+    return hashHex;
+  }
+
+  async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
+    }
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await crypto.subtle.digest("SHA-256", data);
+
+    return base64encode(digest);
+  }
+
+  let codeVerifier = generateRandomString(128);
+
+  generateCodeChallenge(codeVerifier).then(async (codeChallenge) => {
+    let state = generateRandomString(16);
+    let scope = "user-read-private user-read-email";
+
+    let args = new URLSearchParams({
+      response_type: "code",
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirectUri,
+      state: state,
+      code_challenge_method: "S256",
+      code_challenge: codeChallenge,
+    });
+    /*
+    // set cookie with code_verifier
+    setCookie("code_verifier", codeVerifier, { path: "/auth" });
+    */
+    console.log("ARGS:", args);
+    if (browser) {
+      localStorage.setItem("code_verifier", codeVerifier);
+
+      goto("https://accounts.spotify.com/authorize?" + args);
+    }
+
+    // window.location = "https://accounts.spotify.com/authorize?" + args;
+  });
+</script>
+
+<p>client_id {client_id}</p>
+<p>Random string:</p>
---
+++ src/routes/auth/code/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
+  import { claim_svg_element } from "svelte/internal";
+  let code;
+  let codeVerifier;
+  const redirectUri = "http://localhost:5173/auth/code";
+  const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+
+  if (browser) {
+    const urlParams = new URLSearchParams(window.location.search);
+    code = urlParams.get("code");
+
+    codeVerifier = localStorage.getItem("code_verifier");
+
+    const body = new URLSearchParams({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: redirectUri,
+      client_id: client_id,
+      code_verifier: codeVerifier,
+    });
+
+    fetchAccessToken(body);
+  }
+
+  async function fetchAccessToken(body) {
+    const response = fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: body,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("HTTP status " + response.status);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("access_token", data.access_token);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+</script>
+
+<h1>{code}</h1>
