f6f1800da320319d96813b48a08bcbb182d37ff4
hyddeos <ericsageby@hotmail.com>
2023-04-24 22:17:05 +0200

    started with auth component

---
+++ src/components/NotAuthed.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="m-auto pt-[15%]">
+  <h2 class="text-4xl text-center text-light-200 font-bold tracking-wider my-1">
+    First, Spotify Authorization
+  </h2>
+  <p class="text-center">
+    To get started you need to log in to your Spotify Account.
+  </p>
+  <button
+    class="w-28 h-9 m-auto bg-prim-500 rounded text-center text-ellipsis overflow-hidden hover:bg-prim-400"
+    >Log In</button
+  >
+</div>
--- src/routes/coverartgame/+page.svelte
+++ src/routes/coverartgame/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { browser } from "$app/environment";
+  import NotAuthed from "../../components/NotAuthed.svelte";
 
   let bgImage = "/bg.jpg";
   let tapeImage = "/tape.webp";
@@ -8,19 +9,15 @@
   const Client_Secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
   const Client_Refresh_token = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN;
 
-  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${Client_Id}&response_type=code&redirect_uri=http://localhost:5173/coverartgame-auth&scope=user-read-private%20user-read-email`;
-
-  function login() {
-    window.location = AUTH_URL;
-  }
-
   //
   // LAYOUT FOR GAME
   //
+  let authed = false;
   let game_loaded = false;
   let playlist = "";
   let albumcover = "https://www.bengans.se/bilder/artiklar/560029.jpg";
   let album_guess = 99;
+  let displayName = "";
 
   // Temp varibles
   let albums = [
@@ -39,6 +36,7 @@
     album_guess = album_id;
   }
 
+  // Check if logged in and api request
   async function getProfile() {
     if (!browser) return;
 
@@ -46,6 +44,7 @@
 
     if (!accessToken) return;
 
+    authed = true;
     const response = await fetch("https://api.spotify.com/v1/me", {
       headers: {
         Authorization: "Bearer " + accessToken,
@@ -54,6 +53,8 @@
 
     const data = await response.json();
     console.log(data);
+
+    displayName = data.display_name;
   }
 
   getProfile();
@@ -61,77 +62,81 @@
 
 <section id="main_content" class="flex">
   <img id="bg" src={bgImage} alt="consert background" />
-  <div class="m-30 mx-auto w-full">
-    <button on:click={login}>Log in with Spotify</button>
-    <p>Current Guess: {album_guess}</p>
-    <p>Current PL: {playlist}</p>
+  {#if authed}
+    <div class="m-30 mx-auto w-full">
+      <p>Current Guess: {album_guess}</p>
+      <p>Current PL: {playlist}</p>
+      <p>Current display name:{displayName}</p>
 
-    {#if game_loaded}
-      <h2 class="text-5xl text-center font-handwrite tracking-wider my-2">
-        What Album Is This? <span class="text-2xl text-light-300 text-right"
-          >(1/10)</span
-        >
-      </h2>
-      <img
-        class="m-auto p-5 w-1/5"
-        src={albumcover}
-        alt="Guess this album cover"
-      />
-      <div class="w-full flex flex-wrap justify-center px-2">
-        {#each albums as album}
-          <div class="p-2">
-            <button
-              on:click={() => set_active_guess(album.id)}
-              class="w-80 h-40 rounded {album_guess === album.id
-                ? 'bg-dark-200 text-dark-700'
-                : 'bg-dark-700'} text-center text-ellipsis overflow-hidden hover:bg-dark-100 hover:text-dark-700"
-            >
-              <h3 class="font-bold text-xl p-1">
-                {album.album}
-                <h3>
-                  <h4 class="font-thin p-1">{album.artist}</h4>
-                </h3>
-              </h3></button
-            >
-          </div>
-        {/each}
-      </div>
-      <div class="m-auto text-center">
-        <button
-          class="w-40 h-20 rounded {album_guess !== 99
-            ? 'bg-prim-500 text-dark-700'
-            : 'invisible'}  hover:bg-dark-100">Submit</button
-        >
-      </div>
-    {:else}
-      <div class="p-1 0 m-auto mt-40 w-1/3 rounded-md h-96 relative">
-        <img id="tape" src={tapeImage} alt="music tape" />
-        <h2
-          class="text-4xl text-center text-dark-900 font-handwrite tracking-wider my-1 absolute inset-x-0 top-12"
-        >
-          Load Your Playlist!
+      {#if game_loaded}
+        <h2 class="text-5xl text-center font-handwrite tracking-wider my-2">
+          What Album Is This? <span class="text-2xl text-light-300 text-right"
+            >(1/10)</span
+          >
         </h2>
-        <h4
-          class="text-center text-dark-700 text-xl font-thin my-3 absolute inset-x-0 top-20"
-        >
-          Spotify <span class="font-semibold">Playlist-URL</span> or
-          <span class="font-semibold">Playlist-ID</span>
-        </h4>
-        <div class="absolute inset-x-0 bottom-32 m-auto text-center">
-          <input
-            type="text"
-            placeholder="Insert Playlist here"
-            class="p-2 text-dark-900 rounded-md h-10 w-60 border border-light-400 focus:border-sec-500 focus:ring-sec-500"
-            bind:value={playlist}
-          />
+        <img
+          class="m-auto p-5 w-1/5"
+          src={albumcover}
+          alt="Guess this album cover"
+        />
+        <div class="w-full flex flex-wrap justify-center px-2">
+          {#each albums as album}
+            <div class="p-2">
+              <button
+                on:click={() => set_active_guess(album.id)}
+                class="w-80 h-40 rounded {album_guess === album.id
+                  ? 'bg-dark-200 text-dark-700'
+                  : 'bg-dark-700'} text-center text-ellipsis overflow-hidden hover:bg-dark-100 hover:text-dark-700"
+              >
+                <h3 class="font-bold text-xl p-1">
+                  {album.album}
+                  <h3>
+                    <h4 class="font-thin p-1">{album.artist}</h4>
+                  </h3>
+                </h3></button
+              >
+            </div>
+          {/each}
+        </div>
+        <div class="m-auto text-center">
           <button
-            class="w-28 h-9 bg-prim-500 rounded text-center text-ellipsis overflow-hidden hover:bg-prim-400"
-            >Start Game</button
+            class="w-40 h-20 rounded {album_guess !== 99
+              ? 'bg-prim-500 text-dark-700'
+              : 'invisible'}  hover:bg-dark-100">Submit</button
           >
         </div>
-      </div>
-    {/if}
-  </div>
+      {:else}
+        <div class="p-1 0 m-auto mt-40 w-1/3 rounded-md h-96 relative">
+          <img id="tape" src={tapeImage} alt="music tape" />
+          <h2
+            class="text-4xl text-center text-dark-900 font-handwrite tracking-wider my-1 absolute inset-x-0 top-12"
+          >
+            Load Your Playlist!
+          </h2>
+          <h4
+            class="text-center text-dark-700 text-xl font-thin my-3 absolute inset-x-0 top-20"
+          >
+            Spotify <span class="font-semibold">Playlist-URL</span> or
+            <span class="font-semibold">Playlist-ID</span>
+          </h4>
+          <div class="absolute inset-x-0 bottom-32 m-auto text-center">
+            <input
+              type="text"
+              placeholder="Insert Playlist here"
+              class="p-2 text-dark-900 rounded-md h-10 w-60 border border-light-400 focus:border-sec-500 focus:ring-sec-500"
+              bind:value={playlist}
+            />
+            <button
+              class="w-28 h-9 bg-prim-500 rounded text-center text-ellipsis overflow-hidden hover:bg-prim-400"
+              >Start Game</button
+            >
+          </div>
+        </div>
+      {/if}
+    </div>
+  {:else}
+    <NotAuthed />
+  {/if}
 </section>
 
 <style>
